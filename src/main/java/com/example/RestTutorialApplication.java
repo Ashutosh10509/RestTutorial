54affa22e5f19e69ab499a8d6fdecd449d428cd3
Ashutosh10509 <ashutosh10509@gmail.com>
2017-01-07 16:02:21 +0530

    Initial commit

---
+++ src/main/java/com/example/RestTutorialApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class RestTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTutorialApplication.class, args);
+	}
+}
